2d4f2e281cb80e7f6a8c2790c0a97b84c2a48d2f
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-02-05 11:14:17 +0000

    getComputerChoice function coded and tested

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,12 @@
-console.log("Peter is a smart cookie for linking external JavaScript");
+function getComputerChoice() {
+    let choiceNum = Math.floor((Math.random())*3);
+    //choiceNum equals 0,1,2 with equal probability 
+    return((choiceNum === 0) ? "Rock" : ((choiceNum === 1) ? "Paper" : "Scissors"));
+    //so if choiceNum is 0 computer chooses Rock, otherwise if 1 it's Paper, otherwise Scissors
+}
+let i = 1;
+//we will increment i up to 100 to output 100 getComputerChoice values to check it is behaving.
+while (i <= 100) {
+    console.log(getComputerChoice());
+    i += 1;
+}
