69cd3d750c450dadf9e2b3ab48b38f16d6e92ad6
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-02-06 16:15:16 +0000

    Make the game function out of 5 rounds. Appears to work so far, need to deal with plurals

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -24,12 +24,12 @@ function getPlayerChoice() {
 //this next function will assume that capitalisation has already been dealt with by the getPlayerChoice function,
 //so that the playerSelection is definitely "Rock", "Scissors", "Paper" or null.
 
-function rockPaperScissorsRound(playerSelection,computerSelection) {
+function playRound(playerSelection,computerSelection) {
     console.log(playerSelection);
     console.log(computerSelection);
     //testing the input values
     let summary = playerSelection + computerSelection;
-    //a nifty way to test both values consisely, don't you think?
+    //a nifty way to test both values concisely, don't you think?
     if (playerSelection === computerSelection)
     {alert(`That round's a draw because the computer also chose ${playerSelection}.`);
     return "Round drawn";}
@@ -39,10 +39,59 @@ function rockPaperScissorsRound(playerSelection,computerSelection) {
     return "Player wins round";}
     else if (playerSelection === null)
     {alert(`Round voided. Let's try again.`);
-    return "Round void";}
+    return "Void round";}
     alert(`Tough luck, you lost! Computer's ${computerSelection} beats your ${playerSelection}.`)
     return "Computer wins round";
 }
 
-console.log(rockPaperScissorsRound(getPlayerChoice(),getComputerChoice()));
+//console.log(playRound(getPlayerChoice(),getComputerChoice()));
 //combines the above functions to play a round of RPS 
+
+function game() {
+    //create a variable to count the number of the current round
+    let roundNumber = 1;
+    let playerScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+    let voidRounds = 0;
+    while ((roundNumber <= 5) && (voidRounds < 3)) {
+        //now the round takes place, after which, if not voided, the roundNumber increases. So
+        //after round 5 when incremented to 6, the while loop will not execute again.
+        let roundResult = playRound(getPlayerChoice(),getComputerChoice()).charAt(0);
+        //the first character uniquely identifies the result of the round
+        switch (roundResult) {
+            case "V":
+            voidRounds += 1;
+            roundNumber -= 1;
+            //the roundNumber decrements to cancel out the increment that happens at the end of the outer while loop
+            break;
+
+            case "C":
+            computerScore += 1;
+            break;
+
+            case "R":
+            drawScore += 1;
+            break;
+
+            case "P":
+            playerScore += 1;
+            break;
+        }
+    
+     roundNumber += 1;
+        //increments the round number
+    }
+    //after all gameplay, the final result is outputted
+    if (voidRounds > 2)
+    {return `Game voided`;}
+    else if (playerScore > computerScore)
+    {return `Congratulations! You won by ${playerScore} rounds to ${computerScore} with ${drawScore} drawn rounds.`;}
+    else if (playerScore < computerScore)
+    {return `Bested by the computer --- you lost by ${computerScore} rounds to ${playerScore} with ${drawScore} drawn rounds.`;}
+    else
+    {return `It's a draw! You won ${playerScore} rounds and so did the computer, with ${drawScore} drawn rounds.`;}
+    
+
+}
+console.log(game());
