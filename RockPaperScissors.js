ba34c91cd7851a514a4adb689b51543e30a9672e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-02-07 11:38:47 +0000

    Tidy up the code for the game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
     let choiceNum = Math.floor((Math.random())*3);
     //choiceNum equals 0,1,2 with equal probability 
+    
     return((choiceNum === 0) ? "Rock" : ((choiceNum === 1) ? "Paper" : "Scissors"));
     //so if choiceNum is 0 computer chooses Rock, otherwise if 1 it's Paper, otherwise Scissors
 }
@@ -8,12 +9,14 @@ function getComputerChoice() {
 function getPlayerChoice() {
     let rawChoice = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
     //rawChoice must be "rock", "paper", or "scissors" for valid gameplay. Input not case-sensitive.
+      
     if (rawChoice === "rock") return "Rock";
-    else if (rawChoice === "scissors") return "Scissors";
-    else if (rawChoice === "paper") return "Paper";
+        else if (rawChoice === "scissors") return "Scissors";
+        else if (rawChoice === "paper") return "Paper";
     //input converted to a standard state of first letter only being capitalised
-    else alert("That wasn't acceptable. Please type \"Rock\", \"Paper\" or \"Scissors\".");
-    return null;
+        
+        else alert("That wasn't acceptable. Please type \"Rock\", \"Paper\" or \"Scissors\".");
+        return null;
     //null captures all cases of invalid player input
     
 }
@@ -25,23 +28,27 @@ function getPlayerChoice() {
 //so that the playerSelection is definitely "Rock", "Scissors", "Paper" or null.
 
 function playRound(playerSelection,computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection);
+    //console.log(playerSelection);
+    //console.log(computerSelection);
     //testing the input values
+    
     let summary = playerSelection + computerSelection;
     //a nifty way to test both values concisely, don't you think?
+    
     if (playerSelection === computerSelection)
     {alert(`That round's a draw because the computer also chose ${playerSelection}.`);
     return "Round drawn";}
     //the function returns simply the result of the round but the alert message is more descriptive 
-    else if ((summary === "RockScissors") || (summary === "ScissorsPaper") || (summary === "PaperRock"))
-    {alert(`You win the round because ${playerSelection} beats ${computerSelection}!`);
-    return "Player wins round";}
-    else if (playerSelection === null)
-    {alert(`Round voided. Let's try again.`);
-    return "Void round";}
-    alert(`Tough luck, you lost! Computer's ${computerSelection} beats your ${playerSelection}.`)
-    return "Computer wins round";
+      
+      else if ((summary === "RockScissors") || (summary === "ScissorsPaper") || (summary === "PaperRock"))
+         {alert(`You win the round because ${playerSelection} beats ${computerSelection}!`);
+        return "Player wins round";}
+    
+      else if (playerSelection === null)
+        {alert(`Round voided. Let's try again.`);
+        return "Void round";}
+        alert(`Tough luck, you lost! Computer's ${computerSelection} beats your ${playerSelection}.`)
+        return "Computer wins round";
 }
 
 //console.log(playRound(getPlayerChoice(),getComputerChoice()));
@@ -49,16 +56,19 @@ function playRound(playerSelection,computerSelection) {
 
 function game() {
     //create a variable to count the number of the current round
+    
     let roundNumber = 1;
     let playerScore = 0;
     let computerScore = 0;
     let drawScore = 0;
     let voidRounds = 0;
+    
     while ((roundNumber <= 5) && (voidRounds < 3)) {
         //now the round takes place, after which, if not voided, the roundNumber increases. So
         //after round 5 when incremented to 6, the while loop will not execute again.
         let roundResult = playRound(getPlayerChoice(),getComputerChoice()).charAt(0);
         //the first character uniquely identifies the result of the round
+        
         switch (roundResult) {
             case "V":
             voidRounds += 1;
@@ -83,20 +93,24 @@ function game() {
         //increments the round number
     }
     //after all gameplay, the final result is outputted
+    
     if (voidRounds > 2)
     {return `Game voided`;}
-    else if (playerScore > computerScore)
-    {return `Congratulations! You won by ${playerScore} round${(playerScore===1)?'':'s'} to ${computerScore} with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
-    else if (playerScore < computerScore)
-    {return `Bested by the computer --- you lost by ${computerScore} round${(computerScore===1)?'':'s'} to ${playerScore} with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
-    else
-    {return `It's a draw! You won ${playerScore} round${(playerScore===1)?'':'s'} and so did the computer, with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
-    //now the word "round" has an "s" added unless the number of them is exactly 1, using the ternary operator
     
-
+      else if (playerScore > computerScore)
+      {return `Congratulations! You won by ${playerScore} round${(playerScore===1)?'':'s'} to ${computerScore} with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
+    
+      else if (playerScore < computerScore)
+      {return `Bested by the computer --- you lost by ${computerScore} round${(computerScore===1)?'':'s'} to ${playerScore} with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
+    
+      else
+      {return `It's a draw! You won ${playerScore} round${(playerScore===1)?'':'s'} and so did the computer, with ${drawScore} drawn round${(drawScore===1)?'':'s'}.`;}
+      //the word "round" has an "s" added unless the number of them is exactly 1, using the ternary operator
+    
 }
 alert(game());
 
+//THIS SECTION SHOWS HOW I TESTED THE TERNARY OPERATOR CODE FOR PLURALS
 //let test;
 //test = Math.floor(3*Math.random());
 //console.log(test);
